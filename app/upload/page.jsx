427e106c22ab4f3ce827ddd00dc5cc3012095f44
dom <dominicjottaviano@gmail.com>
2025-02-05 00:11:54 -0500

    Upload frontend fully constructed.

--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -1,22 +1,67 @@
 'use client'
 
 import styles from './page.module.css';
+import { useState } from 'react';
+import Image from 'next/image';
 
 const Upload = () => {
 
-	const handleFileChange = (event) => {
+	const [image, setImage] = useState(null);
+
+	const attemptSubmission = () => {
 		
 	}
 
-	return (
+	const getBase64FromUrl = async (imageUrl) => {
+		const response = await fetch(imageUrl);
+		const blob = await response.blob();
 
-		<main className={styles.main}>
-			<div className={styles.interface}>
+		return new Promise((resolve) => {
+			const reader = new FileReader();
+			reader.onloadend = () => resolve(reader.result);
+			reader.readAsDataURL(blob);
+		});
+	}
+
+	const handleFileChange = async (event) => {
+		if (event.target.files && event.target.files.length > 0) {
+			const file = event.target.files[0];
+
+			// Check if the file is an image
+			if (!file.type.startsWith("image/")) {
+				alert("Please upload an image file.");
+				return;
+			}
+
+			// Store the image as a preview URL
+			const imageUrl = URL.createObjectURL(file);
+			const base64 = await getBase64FromUrl(imageUrl);
+			setImage(base64);
+			document.getElementById('submit').disabled = false;
+			document.getElementById('submit').addEventListener('click', attemptSubmission);
+
+		}
+	};
+
+	const Submission = () => {
+
+		if(image) {
+			console.log(image);
+			return (
+				<Image 
+					className={styles.img} 
+					src={image}
+					alt='Uploaded picture'
+					width={window.innerWidth*0.4}
+					height={window.innerWidth*0.4}
+				/>
+			);
+		}
+
+		return (
+			<div>
 				<label htmlFor='file-upload' className={styles.input}>
-					<div className={styles.inputImage} />
-					<div className={styles.inputLabel}>
-						Select an image :3
-					</div>
+					<p className={styles.inputLabel}>Select an image :3</p>
 				</label>
 				<input
 					id='file-upload'
@@ -24,6 +69,16 @@ const Upload = () => {
 					onChange={handleFileChange}
 					hidden
 				/>
+			</div>
+		);
+	}
+
+	return (
+
+		<main className={styles.main}>
+			<div className={styles.submission}>
+				<Submission />
+				<button className={styles.submitButton} id='submit' disabled>Submit</button>
 			</div>
 		</main>
 
